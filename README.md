065a9f8392e446d838f431a07da66db32efa8ca3
Joost Nieuwenhuijse <joost@newhouse.nl>
2018-08-28 23:43:00 +0200

    Update build script

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ I/O expander for Zynthian using USB connected STM32 microcontroller
 
 # Install host software
 Connect via SSH to your zythian box and enter:
+
 ```
 cd /zynthian
 git clone --recursive https://github.com/joostn/zynthian-stm32controller.git
 cd zynthian-stm32controller
 ./build.sh
 ```
+
+This will build the host software and install as /etc/systemd/system/zynthian-stm32controller-host.service.
--- build.sh
+++ build.sh
@@ -17,3 +17,21 @@ fi
 cd build
 cmake ../host
 make
+
+cat << EOF > /etc/systemd/system/zynthian-stm32controller-host.service
+[Unit]
+Description=I/O expander for Zynthian using USB connected STM32 microcontroller
+
+[Service]
+Environment=HOME=/root
+WorkingDirectory=/root
+ExecStart=/zynthian/zynthian-stm32controller/build/zynthian-stm32controller-host
+Restart=always
+RestartSec=1
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+systemctl start zynthian-stm32controller-host.service
+systemctl enable zynthian-stm32controller-host.service
